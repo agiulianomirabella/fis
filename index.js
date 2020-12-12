897dcf30c57dbd634579bdfc1f962e049578f953
gmirabella <giulianomirabella@gmail.com>
2020-12-12 22:20:08 +0000

    Added support to nebd POST method

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var DataStore = require('nedb')
 
 var port = 3000;
 var BASE_API_PATH = "/api/v1";
-
-var products = [
-    {"name": "Peter", "phone": "12345"},
-    {"name": "John", "phone": "98765"}
-];
+var DB_FILE_NAME = __dirname + "/products.json";
 
 console.log("Starting API server...");
 
 var app = express();
 app.use(bodyParser.json());
+
+var db = new DataStore({
+    filename: DB_FILE_NAME,
+    autoload: true
+});
 
 app.get("/", (req, res) => {
     res.send("<html><body><h1>My server</h1></body></html>");
@@ -20,17 +22,21 @@ app.get("/", (req, res) => {
 
 app.get(BASE_API_PATH + "/products", (req, res) => {
     console.log(Date() + " - GET /products");
-    res.send(products);
+    res.send([]);
 });
 
 app.post(BASE_API_PATH + "/products", (req, res) => {
     console.log(Date() + " - POST /products");
     var product = req.body;
-    products.push(product);
-    res.sendStatus(201);
+    db.insert(product, (err) => {
+        if (err) {
+            console.log(Date() + " - " + err);
+            res.sendStatus(500);
+        } else {
+            res.sendStatus(201);
+        }
+    });
 });
-
-
 
 app.listen(port);
 
