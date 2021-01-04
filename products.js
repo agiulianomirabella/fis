4fb66417c9a5ff5e45042fe945dc95d59dfda4df
lola <lolagomezjemes@gmail.com>
2021-01-04 11:38:01 +0000

    Add new fields to product

--- products.js
+++ products.js
@@ -2,14 +2,16 @@ const mongoose = require('mongoose');
 
 
 const contactSchema = new mongoose.Schema({
-    id: Number , //número interno
     code: String, //código de barras
     name: String,
     description: String,
+    productImages: Buffer,
+    provider: String,
     category: String,
     price: Number,
     stock: Boolean,
-    amount: Number
+    amount: Number,
+
 
 })
 
@@ -18,6 +20,8 @@ contactSchema.methods.cleanup = function(){
     return{code: this.code,
         name: this.name,
         description: this.description,
+        productImages: this.productImages,
+        provider: this.provider,
         category: this.category,
         price: this.price,
         stock: this.stock,
