4b000e9fe6c9e374e08356ba65f98442e2afa666
lola <lolagomezjemes@gmail.com>
2021-01-20 17:38:57 +0000

    Addition of authentication in api methods

---
+++ setupbd.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const dbconnect = require('./db');
+const ApiKey = require('./apikeys');
+const dbConnect = require('./db');
+
+dbConnect().then(
+    ()=>{
+        const user= new ApiKey({user:'fis'});
+        user.save(function(err, user){
+            if(err){
+                console.log(err);
+            } else{
+                console.log('User: '+ user.user + ", "+ user.apikey+ "saved");
+            }
+
+        })
+    }
+)
