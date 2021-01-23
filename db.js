d9918bfb37f6fcbe51989707a390615eded88a8e
lola <lolagomezjemes@gmail.com>
2021-01-23 21:17:45 +0000

    change db_url

--- db.js
+++ db.js
@@ -1,9 +1,7 @@
 const mongoose= require('mongoose');
 
 
-
-const DB_URL=(process.env.MONGODB_URI || 'http://host.docker.internal:3000/api/v2');
-// const DB_URL=(process.env.MONGODB_URI || 'mongodb://db/test');
+const DB_URL=(process.env.MONGODB_URI || 'mongodb://db/test');
 
 const dbConnect= function(){
     const db=mongoose.connection; 
