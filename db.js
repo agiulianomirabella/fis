235c6de9a940e2ad13a640b5260814a0bec62525
Giuliano Mirabella <giulianomirabella@gmail.com>
2021-01-26 23:07:06 +0000

    useUnifiedTopology

--- db.js
+++ db.js
@@ -6,8 +6,8 @@ const DB_URL=(process.env.MONGODB_URI || 'mongodb://db/test');
 const dbConnect= function(){
     const db=mongoose.connection; 
     db.on('error', console.error.bind(console, 'connection error: ')); 
-    return mongoose.connect(DB_URL,{useNewUrlParser:true}); 
-    
+    return mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+    // return mongoose.connect(DB_URL,{useNewUrlParser:true});
 }
 
 module.exports =dbConnect;
--- tests/integration-db.test.js
+++ tests/integration-db.test.js
@@ -1,51 +1,41 @@
 const Product = require('../products.js');
 const mongoose = require('mongoose');
-const dbConnect = require('../db.js')
+const dbConnect = require('../db')
 
-describe("Hello world tests", () => {
-    it("should do a stupid test", () => {
-        const a = 5;
-        const b = 3;
-        const sum = a + b;
-
-        expect(sum).toBe(8);
+describe('Products DB connection', () => {
+    beforeAll(()=> {
+        return dbConnect();
     });
-});
 
-// describe('Products DB connection', () => {
-//     beforeAll(()=> {
-//         return dbConnect();
-//     });
+    beforeEach((done) => {
+        Product.deleteMany({}, (err) => {
+            done();
+        });
+    });
 
-//     beforeEach((done) => {
-//         Product.deleteMany({}, (err) => {
-//             done();
-//         });
-//     });
+    it('Writes a product in the DB', (done) => {
+        const product = new Product({
+            "code": "code_integration_test",
+            "name": "product_integration_test",
+            "provider_name": "provider_name_integration_test",
+            "provider_cif": "provider_cif_integration_test",
+            "category": "Guantes",
+            "price": 50,
+            "amount": 200
+        });
+        product.save((err, product) => {
+            expect(err).toBeNull();
+            Product.find({}, (err, products) => {
+                expect(products).toBeArrayOfSize(1);
+                done();
+            });
+        });
+    });
 
-//     it('Writes a product in the DB', (done) => {
-//         const product = new Product({
-//             "code": "code_integration_test",
-//             "name": "product_integration_test",
-//             "provider_name": "provider_name_integration_test",
-//             "provider_cif": "provider_cif_integration_test",
-//             "category": "Guantes",
-//             "price": 50,
-//             "amount": 200
-//         });
-//         product.save((err, product) => {
-//             expect(err).toBeNull();
-//             Product.find({}, (err, products) => {
-//                 expect(products).toBeArrayOfSize(1);
-//                 done();
-//             });
-//         });
-//     });
+    afterAll((done) => {
+        mongoose.connection.db.dropDatabase(() => {
+            mongoose.connection.close(done);
+        });
+    });
 
-//     afterAll((done) => {
-//         mongoose.connection.db.dropDatabase(() => {
-//             mongoose.connection.close(done);
-//         });
-//     });
-
-// })
+})
