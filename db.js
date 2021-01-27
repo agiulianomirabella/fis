5ddb71fbb59eb7793219e3d5acb6f7d46f138cbf
Giuliano Mirabella <giulianomirabella@gmail.com>
2021-01-27 00:12:30 +0000

    console.log DB_URL

--- db.js
+++ db.js
@@ -1,27 +1,28 @@
-const mongoose = require('mongoose');
-const DB_URL = (process.env.MONGODB_URI || 'mongodb://db/test');
-// const DB_URL = ("mongodb+srv://user0:etwB8JAXa2aTPokB@product.8m7zl.mongodb.net/Products?retryWrites=true&w=majority" || 'mongodb://db/test');
+// const mongoose = require('mongoose');
+// const DB_URL = (process.env.MONGODB_URI || 'mongodb://db/test');
+// // const DB_URL = ("mongodb+srv://user0:etwB8JAXa2aTPokB@product.8m7zl.mongodb.net/Products?retryWrites=true&w=majority" || 'mongodb://db/test');
 
-const dbConnect = function() {
-    const db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error: '));
-    return mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-}
-
-module.exports = dbConnect;
-
-
-
-// const mongoose= require('mongoose');
-
-// // const DB_URL=(process.env.MONGODB_URI || 'http://host.docker.internal:3000/api/v2');
-// const DB_URL=(process.env.MONGODB_URI || 'mongodb://db/test');
-
-// const dbConnect= function(){
-//     const db=mongoose.connection; 
-//     db.on('error', console.error.bind(console, 'connection error: ')); 
+// const dbConnect = function() {
+//     const db = mongoose.connection;
+//     db.on('error', console.error.bind(console, 'connection error: '));
 //     return mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-//     // return mongoose.connect(DB_URL,{useNewUrlParser:true});
 // }
 
-// module.exports =dbConnect;
+// module.exports = dbConnect;
+
+
+
+const mongoose= require('mongoose');
+
+// const DB_URL=(process.env.MONGODB_URI || 'http://host.docker.internal:3000/api/v2');
+const DB_URL=(process.env.MONGODB_URI || 'mongodb://db/test');
+
+const dbConnect= function(){
+    console.log(DB_URL);
+    const db=mongoose.connection; 
+    db.on('error', console.error.bind(console, 'connection error: ')); 
+    return mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+    // return mongoose.connect(DB_URL,{useNewUrlParser:true});
+}
+
+module.exports =dbConnect;
